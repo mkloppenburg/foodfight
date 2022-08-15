e2e4e1215d65efd0f745d61dd2fcca10d37d5ce3
mkloppenburg <34100186+mkloppenburg@users.noreply.github.com>
2022-08-15 21:45:31 +0200

    Delete games directory

--- games/admin.py
+++
@@ -1,7 +0,0 @@
-""" file for registering games models to the admin """
-from django.contrib import admin
-from .models import Item, PlayerItem
-
-# Register your models here.
-admin.site.register(Item)
-admin.site.register(PlayerItem)
