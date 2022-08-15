cbfbb9207fa2ef52f95089624b457c7813b5bf09
mkloppenburg <34100186+mkloppenburg@users.noreply.github.com>
2022-08-15 21:45:41 +0200

    Delete player directory

--- player/admin.py
+++
@@ -1,8 +0,0 @@
-""" Admin extensions for the player section """
-from django.contrib import admin
-from .models import Player, Rank
-
-# Register your models here.
-
-admin.site.register(Player)
-admin.site.register(Rank)
