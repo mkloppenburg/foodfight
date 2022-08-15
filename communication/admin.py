f8aaee4cd94e7e191bb6ba42a23cc6346b0be0fe
mkloppenburg <34100186+mkloppenburg@users.noreply.github.com>
2022-08-15 21:44:53 +0200

    Delete communication directory

--- communication/admin.py
+++
@@ -1,6 +0,0 @@
-""" file for registering communication models to the admin """
-from django.contrib import admin
-from .models import Message
-
-# Register your models here.
-admin.site.register(Message)
